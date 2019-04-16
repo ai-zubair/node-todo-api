7f942aaea65c36cf65ba9401f7a0682648c35f6a
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-16 23:20:24 +0530

    add the users collection

--- playground/mongo-db-connect.js
+++ playground/mongo-db-connect.js
@@ -9,7 +9,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',{useNewUrlParser : true}
     const db = client.db();
     db.collection('Todos',(err,collection)=>{
         if(err){
-            console.log('Ah! Snap! An Error occurred creating the collection!');
+            console.log(`Ah! Snap! An Error occurred creating the collection ${collection.s.name}!`);
             return;
         }
         collection.insertOne({
@@ -17,10 +17,27 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',{useNewUrlParser : true}
             status : 'Incomplete'
         },(err,result)=>{
             if(err){
-                console.log('Ah! Snap! An Error occurred writing documments into the collection!');
+                console.log(`Ah! Snap! An Error occurred writing documment(s) into the collection ${collection.s.name}`);
                 return;
             }
-            console.log(`${result.insertedCount} record(s) successfully inserted:\n${JSON.stringify(result.ops,undefined,2)} `)
+            console.log(`${result.insertedCount} record(s) successfully inserted into ${collection.s.name} collection:\n${JSON.stringify(result.ops,undefined,2)} `)
+        })
+    })
+    db.collection('Users',(err,collection)=>{
+        if(err){
+            console.log(`Ah! Snap! An Error occurred creating the collection ${collection.s.name}!`);
+            return;            
+        }
+        collection.insertOne({
+            name: 'zubair',
+            email: 'zubair@gmail.com',
+            password : 'thisIsJustMe'
+        },(err,result)=>{
+            if(err){
+                console.log(`Ah! Snap! An Error occurred writing documment(s) into the collection ${collection.s.name}`);
+                return;
+            }
+            console.log(`${result.insertedCount} record(s) successfully inserted into ${collection.s.name} collection:\n${JSON.stringify(result.ops,undefined,2)} `)
         })
     })
     client.close();
