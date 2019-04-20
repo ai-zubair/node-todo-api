d2f1dfff485741281ccaf281f71cc4da91656337
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-21 02:32:46 +0530

    add the users model

--- server/server.js
+++ server/server.js
@@ -10,24 +10,48 @@ mongoose.connect("mongodb://localhost:27017/TodoApp",{useNewUrlParser : true})
 var todosSchema = new mongoose.Schema({
     text:{
         type: String,
+        minlength : 1,
+        trim : true,
         required: true
     },
     status : {
         type: Boolean,
-        required : true
+        default : false
+    }
+})
+
+//creating a schema for the documents of the users collection
+var userSchema = new mongoose.Schema({
+    email : {
+        type : String,
+        required : true,
+        minlength : 1,
+        trim : true
     }
 })
 
 //creating a model for the todo collection to attach the schema to all of its documents
-var todos = mongoose.model('Todos', todosSchema);
+var Todos = mongoose.model('Todos', todosSchema);
+
+//cerating a users model
+var Users = mongoose.model('Users',userSchema);
 
 //creating a new document in the todos collection using the model of the same
-var newTodo = new todos({
-    text:'this is a mongoose test',
-    status: false
+var newTodo = new Todos({
+    text:'t'
+})
+
+//creating a new user document
+var newUser = new Users({
+    email : 'zubair@mail.com'
 })
 
 //saving the document into the todos collection
 newTodo.save().then((todo)=>{
     console.log('Saved the todo:\n',todo)
 }).catch(err=>console.log(err));
+
+//saving the document into the users collection
+newUser.save().then((user)=>{
+    console.log('Saved the user:\n',user)
+})
