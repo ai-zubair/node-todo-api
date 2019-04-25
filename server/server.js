5d48d3b4546b47d5149352e460fabf58f99b5af1
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-25 16:47:28 +0530

    add comments for server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const { ObjectID } = require('mongodb');
+const bodyParser = require('body-parser'); //parse request bodies on to the request object as req.body
+const { ObjectID } = require('mongodb'); //validate the object id passed in the URL
 
 const { mongoose } = require('./db/mongoose'); //requiring the confgiured mongoose variable
-const { Todos } = require('./db-models/todos');
-const { Users } = require('./db-models/users');
+const { Todos } = require('./db-models/todos'); //Todos model for the db
+const { Users } = require('./db-models/users'); //Users model for the db
 
+//configuring the prot for heroku
 const PORT = process.env.PORT || 3000
 
+//create the express appp
 var app =  express();
 
+//using the body parser middleware function to parse request bodies as json
 app.use(bodyParser.json());
 
+//set up POST route for adding todos to the TODO's collectio for the database
 app.post('/todos',(req,res,next)=>{
     const newTodo = new Todos({
         text : req.body.text,
@@ -24,6 +28,7 @@ app.post('/todos',(req,res,next)=>{
     })
 })
 
+//set up GET route for fetching todos from the todos collection
 app.get('/todos',(req,res,next)=>{
     Todos.find().then( todos=>{
         res.send({
@@ -34,6 +39,7 @@ app.get('/todos',(req,res,next)=>{
     })
 })
 
+//set up GET route for fetching todo for a given  id
 app.get('/todos/:id',(req,res,next)=>{
     const taskID = req.params.id;
     if(!ObjectID.isValid(taskID)){
@@ -49,6 +55,7 @@ app.get('/todos/:id',(req,res,next)=>{
     })
 })
 
+//set up the server to listen for connections on the specified port
 app.listen(PORT,()=>{
     console.log(`Server has been fired at localhost:${PORT}`)
 })
