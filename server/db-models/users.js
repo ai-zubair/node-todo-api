e1e6eb7d89d535834bd3a35f9c63837ef082490c
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-04-27 19:27:52 +0530

    set up the users model for the db

--- server/db-models/users.js
+++ server/db-models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const isEmail =  require('validator/lib/isEmail');
 
 //creating a schema for the documents of the users collection
 const userSchema = new mongoose.Schema({
@@ -6,8 +7,31 @@ const userSchema = new mongoose.Schema({
         type : String,
         required : true,
         minlength : 1,
-        trim : true
-    }
+        trim : true,
+        unique : true,
+        validate:{
+            validator : isEmail,
+            message: props => `${props.value} is not a valid email.`
+        }
+    },
+    password : {
+        type : String,
+        required : true,
+        trim : true,
+        minlength : 6
+    },
+    tokens : [
+        {
+            access : {
+                type : String,
+                required : true
+            },
+            token : {
+                type : String,
+                required : true
+            }
+        }
+    ]
 })
 //creating a users model
 const Users = mongoose.model('Users',userSchema);
