048d63cb810a965a27bebea1a2f0e3687ee37b10
Zubair Bashir <zubair.bashirzubair@gmail.com>
2019-05-02 15:12:45 +0530

    todos schema changed for efficiency

--- server/db-models/todos.js
+++ server/db-models/todos.js
@@ -2,20 +2,22 @@ const mongoose = require('mongoose');
 
 //creating a schema for the documents of the todo collection
 const TodosSchema = new mongoose.Schema({
-    text:{
-        type: String,
-        minlength : 1,
-        trim : true,
-        required: true
+    _creator : {
+        type: mongoose.Schema.Types.ObjectId
     },
-    status : {
-        type: Boolean,
-        default : false
-    },
-    completedAt:{
-        type : String,
-        default : 'Not completed yet'
-    }
+    savedTodos:[{
+        title:{
+            type: String,
+            minlength : 1,
+            trim : true,
+            required: true
+        },
+        status : {
+            type: Boolean,
+            default : false
+        }
+    }]
+    
 })
 //creating a model for the todo collection to attach the schema to all of its documents
 const Todos = mongoose.model('Todos', TodosSchema);
